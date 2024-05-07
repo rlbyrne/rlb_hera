e4e7702ec6d74e93333855bae3a8c6fcfc1f9f2b
Ruby Byrne <rbyrne@caltech.edu>
2024-05-06 17:13:59 -0700

    run abscal on all data files

--- run_abscal.py
+++ run_abscal.py
@@ -1,48 +1,67 @@
 import numpy as np
 import pyuvdata
 from newcal import calibration_wrappers
-import sys
+import os
 
-def test_abscal_Apr23():
 
-    datafile_name = "zen.2459861.52096.sum.abs_calibrated.red_avg"
-    data_path = f"/safepool/rbyrne/hera_data/H6C-data/2459861/{datafile_name}.uvh5"
-    model_path = f"/safepool/rbyrne/hera_data/interpolated_models/{datafile_name}_model.uvfits"
-    data = pyuvdata.UVData()
-    data.read(data_path)
-    model = pyuvdata.UVData()
-    model.read(model_path)
+def run_abscal():
 
-    data.inflate_by_redundancy(use_grid_alg=True)
-    model.inflate_by_redundancy(use_grid_alg=True)
+    model_filepath = "/safepool/rbyrne/hera_data/interpolated_models"
+    data_filepath = "/safepool/rbyrne/hera_data/H6C-data/2459861"
+    output_path = "/safepool/rbyrne/hera_abscal"
+    model_filenames = os.listdir(model_filepath)
+    datafile_names = [name.removesuffix("_model.uvfits") for name in model_filenames]
 
-    # Model does not include all baselines
-    model_baselines = list(set(list(zip(model.ant_1_array, model.ant_2_array))))
-    data_baselines = list(set(list(zip(data.ant_1_array, data.ant_2_array))))
-    use_baselines = [baseline for baseline in model_baselines if (baseline in data_baselines) or (baseline[::-1] in data_baselines)]
-    use_polarizations = [pol for pol in model.polarization_array if pol in data.polarization_array]
-    data.select(bls=use_baselines, polarizations=use_polarizations)
-    model.select(bls=use_baselines, polarizations=use_polarizations)
+    for file_ind, datafile_name in enumerate(datafile_names):
 
-    # Align phasing
-    data.phase_to_time(np.mean(data.time_array))
-    model.phase_to_time(np.mean(data.time_array))
+        print(f"Processing file {file_ind+1} of {len(datafile_names)}")
 
-    #null = data.sum_vis(model, inplace=False, override_params=["nsample_array", "earth_omega", "flag_array", "filename", "phase_center_catalog", "timesys"])  # Verify that metadata match
+        data_path = f"{data_filepath}/{datafile_name}.uvh5"
+        model_path = f"{model_filepath}/{datafile_name}_model.uvfits"
+        data = pyuvdata.UVData()
+        data.read(data_path)
+        model = pyuvdata.UVData()
+        model.read(model_path)
 
-    abscal_params = calibration_wrappers.absolute_calibration(
-        data,
-        model,
-        log_file_path="/safepool/rbyrne/hera_abscal/calibration_logs/abscal_test_May3.txt",
-    )
-    print(abscal_params[0,:,0])
+        data.inflate_by_redundancy(use_grid_alg=True)
+        model.inflate_by_redundancy(use_grid_alg=True)
 
-    data = pyuvdata.UVData()
-    data.read(data_path)
-    data.phase_to_time(np.mean(data.time_array))
-    calibration_wrappers.apply_abscal(data, abscal_params, data.polarization_array, inplace=True)
-    data.write_uvfits(f"/safepool/rbyrne/hera_abscal/{datafile_name}_abscal.uvfits")
+        # Model does not include all baselines
+        model_baselines = list(set(list(zip(model.ant_1_array, model.ant_2_array))))
+        data_baselines = list(set(list(zip(data.ant_1_array, data.ant_2_array))))
+        use_baselines = [
+            baseline
+            for baseline in model_baselines
+            if (baseline in data_baselines) or (baseline[::-1] in data_baselines)
+        ]
+        use_polarizations = [
+            pol for pol in model.polarization_array if pol in data.polarization_array
+        ]
+        data.select(bls=use_baselines, polarizations=use_polarizations)
+        model.select(bls=use_baselines, polarizations=use_polarizations)
+
+        # Align phasing
+        data.phase_to_time(np.mean(data.time_array))
+        model.phase_to_time(np.mean(data.time_array))
+
+        # null = data.sum_vis(model, inplace=False, override_params=["nsample_array", "earth_omega", "flag_array", "filename", "phase_center_catalog", "timesys"])  # Verify that metadata match
+
+        abscal_params = calibration_wrappers.absolute_calibration(
+            data,
+            model,
+            log_file_path=f"{output_path}/calibration_logs/{datafile_name}.txt",
+        )
+
+        data = pyuvdata.UVData()
+        data.read(data_path)
+        data.phase_to_time(np.mean(data.time_array))
+        calibration_wrappers.apply_abscal(
+            data, abscal_params, data.polarization_array, inplace=True
+        )
+        data.write_uvfits(f"{output_path}/{datafile_name}_abscal.uvfits")
+        with open(f"{output_path}/{datafile_name}_abscal_params.npy", "wb") as f:
+            np.save(f, abscal_params)
 
 
 if __name__ == "__main__":
-    test_abscal_Apr23()
+    run_abscal()
